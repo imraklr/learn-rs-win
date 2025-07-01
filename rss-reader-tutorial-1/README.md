228da047785659dbd0d8c5b0899bbb973e8462fe
Rakesh Kumar <44721620+imraklr@users.noreply.github.com>
2025-07-01 11:44:02 +0530

    new: add rss-reader tutorial code

---
+++ rss-reader-tutorial-1/README.md
@@ -0,0 +1,4 @@
+# rss_feed
+
+This project is available on [link](https://learn.microsoft.com/en-us/windows/dev-environment/rust/rss-reader-rust-for-windowshttps://learn.microsoft.com/en-us/windows/dev-environment/rust/rss-reader-rust-for-windows) with all the steps needed to get started with Windows with Rust programming language.
+
---
+++ rss-reader-tutorial-1/src/main.rs
@@ -0,0 +1,18 @@
+use windows::{core::*, Foundation::Uri, Web::Syndication::{SyndicationClient}};
+
+fn main() -> Result<()> {
+    let uri = Uri::CreateUri(h!("https://blogs.windows.com/feed"))?;
+    let client = SyndicationClient::new()?;
+
+    let _ = client.SetRequestHeader(
+        h!("User-Agent"),h!("Mozilla/5.0 (compatible; MSIE 10.0; Windows NT 6.2; WOW64; Trident/6.0)")
+    );
+
+    let feed = client.RetrieveFeedAsync(&uri)?.get()?;
+
+    for item in feed.Items()? {
+        println!("{}", item.Title()?.Text()?);
+    }
+
+    Ok(())
+}
